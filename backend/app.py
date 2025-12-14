7e797d2da8cb419c0d37bc3138327ac80a3aba51
10bags <159627591+10bags@users.noreply.github.com>
2025-12-15 01:01:24 +0800

    HOTFIX

--- backend/app.py
+++ backend/app.py
@@ -1,44 +1,44 @@
 import json
 import boto3
+import os
 import time
 
-dynamodb = boto3.resource('dynamodb')
 transcribe = boto3.client('transcribe')
+dynamodb = boto3.resource('dynamodb')
 
-TABLE_NAME = "Transcriptions"
-BUCKET_NAME = "smartboard-audio-matin09-858039354211"
+TABLE_NAME = os.environ['TABLE_NAME']
+BUCKET_NAME = os.environ['BUCKET_NAME']
 
 def lambda_handler(event, context):
-    params = event.get("queryStringParameters") or {}
-    file_name = params.get("filename")
+
+    params = event.get('queryStringParameters') or {}
+    file_name = params.get('filename')
 
     if not file_name:
         return {"statusCode": 400, "body": "Missing filename"}
 
+    job_name = f"transcribe-{int(time.time())}"
     s3_uri = f"s3://{BUCKET_NAME}/{file_name}"
-    job_name = f"transcription-{int(time.time())}"
 
     transcribe.start_transcription_job(
         TranscriptionJobName=job_name,
-        Media={"MediaFileUri": s3_uri},
-        MediaFormat="mp3",
-        LanguageCode="en-US"
+        Media={'MediaFileUri': s3_uri},
+        MediaFormat='mp3',
+        LanguageCode='en-US'
     )
 
     table = dynamodb.Table(TABLE_NAME)
-    table.put_item(
-        Item={
-            "FileName": file_name,
-            "JobName": job_name,
-            "Status": "IN_PROGRESS",
-            "Timestamp": str(int(time.time()))
-        }
-    )
+    table.put_item(Item={
+        'FileName': file_name,
+        'JobName': job_name,
+        'Status': 'STARTED',
+        'Timestamp': int(time.time())
+    })
 
     return {
         "statusCode": 200,
         "body": json.dumps({
             "message": "Transcription started",
-            "job": job_name
+            "job_name": job_name
         })
     }
